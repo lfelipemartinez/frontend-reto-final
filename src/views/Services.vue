ee31cdeb976b9f00dba25261ebf96e93f6d0cf8f
Luis <lfelipemartinez07@gmail.com>
2020-12-23 08:22:24 -0500

    vista servicios

---
+++ src/views/Services.vue
@@ -0,0 +1,19 @@
+<template>
+
+    
+    <div>
+        <Header></Header>
+         <FooterV></FooterV>
+    </div>
+   
+</template>
+
+<script>
+import Header from "../components/header.vue";
+import Footer from "../components/FooterV.vue";
+import FooterV from '../components/FooterV.vue';
+export default {
+  components: { Header, FooterV},
+    name:"Services"
+}
+</script>
